5915d52cd8e8f4367876e0a6e933fe3f31e7f5c6
Emily Valdez <emily.milan.valdez@gmail.com>
2023-12-04 10:32:59 -0600

    installeld material ui

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import App from "./components/App";
 import "./index.css";
 import { createRoot } from "react-dom/client";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const container = document.getElementById("root");
 const root = createRoot(container);
