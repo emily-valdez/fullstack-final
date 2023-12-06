4f746dc4954cf4befc6cdf877a49cb4b6aa94e0c
Emily Valdez <emily.milan.valdez@gmail.com>
2023-12-06 12:02:13 -0600

    added model relationships

--- server/models.py
+++ server/models.py
@@ -1,9 +1,19 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 
-from config import db
+from config import db, metadata
 
-# Models go here!
+# Association table for relationship between users and books
+users_books = db.Table(
+    'users_books',
+    metadata,
+    db.Column('users_id', db.Integer, db.ForeignKey(
+        'users.id'), primary_key=True),
+    db.Column('books_id', db.Integer, db.ForeignKey(
+        'books.id'), primary_key=True)
+)
+
+# Models
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
     
@@ -11,32 +21,26 @@ class User(db.Model, SerializerMixin):
     
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String)
+    #password_hash
     
     # Add relationships
+    books = db.relationship(
+        'Book', secondary=users_books, back_populates='users', cascade='all, delete-orphhan'
+    )
     
     # Add validations
     def validate_username(self, key, username):
         if len(username) > 1:
             return username
         else:
-            raise ValueError('Name must be more than one character.')
-        
-class Author(db.Model, SerializerMixin):
-    __tablename__ = 'authors'
+            raise ValueError('Username must be longer than one character.')
     
-    # Add serialization rules
-    
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String)
-    publisher = db.Column(db.String)
-    tiktok = db.Column(db.String)
-    
-    # Add relationships
-    
-    # Add validations
-        
+    def __repr__(self):
+        return f'<User {self.id}, {self.username}>'
+       
 
-class Books(db.Model, SerializerMixin):
+
+class Book(db.Model, SerializerMixin):
     __tablename__ = 'books'
     
     # Add serialization rules
@@ -44,9 +48,15 @@ class Books(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String)
     year = db.Column(db.Integer)
-    author_name = db.Column(db.String, db.ForeignKey('authors.name'))
+    author_id = db.Column(db.String, db.ForeignKey('authors.id'))
+    heart_count =db.Column(db.Integer)
+    pepper_count =db.Column(db.Integer)
     
     # Add relationships
+    users = db.relationship(
+        'User', secondary=users_books, back_populates='users', cascade='all, delete-orphhan'
+    )
+    author = db.relationship('Author', back_populates="books")
     
     # Add validations
     def validate_title(self, key, title):
@@ -60,3 +70,23 @@ class Books(db.Model, SerializerMixin):
             return year
         else:
             raise ValueError('Year must be four numbers.')
+        
+    def __repr__(self):
+        return f'<Books {self.id}, {self.title}, {self.year}>'
+    
+    
+class Author(db.Model, SerializerMixin):
+    __tablename__ = 'authors'
+    
+    # Add serialization rules
+    
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String)
+    publisher = db.Column(db.String)
+    tiktok = db.Column(db.String)
+    
+    # Add relationships
+    books = db.relationship('Book')
+        
+    def __repr__(self):
+        return f'<Author {self.id}, {self.name}, {self.publisher}, {self.tiktok}>'
